f94473d008c3b2d0d366ee1dee1daa2a3e913e87
Muzim Qureshi <muzimqureshi786@gmail.com>
2023-08-26 18:58:47 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
+document.getElementById("tab2").addEventListener("click",()=>{
+	document.getElementById("tab-2-content").style.display = "block";
+});
+document.getElementById("tab3").addEventListener("click",()=>{
+	document.getElementById("tab-3-content").style.display = "block";
+});
+document.getElementById("tab1").addEventListener("click",()=>{
+	document.getElementById("tab-1-content").style.display = "block";
+});
